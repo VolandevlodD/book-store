c616db652c76b011a473a50959b97e83a6f2af1a
VolandevlodD <vladradko2000@gmail.com>
2024-04-29 20:58:57 +0300

    Refactored based on review

--- src/main/java/org/example/bookstore/model/Book.java
+++ src/main/java/org/example/bookstore/model/Book.java
@@ -15,8 +15,6 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted = false")
 public class Book {
-    @Column(nullable = false, name = "is_deleted")
-    private boolean isDeleted = false;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
