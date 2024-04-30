d6b28b259ea793d84036ad366c59fef2a4b97584
VolandevlodD <vladradko2000@gmail.com>
2024-04-30 19:29:01 +0300

    resolving conflicts

--- src/main/java/org/example/bookstore/exception/CustomGlobalExceptionHandler.java
+++ src/main/java/org/example/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package org.example.bookstore.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomGlobalExceptionHandler {
