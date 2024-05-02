69aa391550dd8ff3a2f58a1e57ffadf39d80b1ac
Vladyslav Radko <124697095+VolandevlodD@users.noreply.github.com>
2024-05-02 16:41:44 +0300

    Global exception handler and data validation (#4)
    * repository and db changes
    
    changed book repository impl to jpa
    added liquibase dependency
    added ability to rollback changesets
    book.id added to ignore in mapper
    added 1 changeset for book creation
    
    * Implement JpaRepository, Liquibase, Soft Delete, and Additional Book Endpoints
    
    * Update BookMapper.java
    
    * Refactored based on review
    
    * validation and emty CustomGlobalException class was added
    
    * repository and db changes
    
    changed book repository impl to jpa
    added liquibase dependency
    added ability to rollback changesets
    book.id added to ignore in mapper
    added 1 changeset for book creation
    
    * Implement JpaRepository, Liquibase, Soft Delete, and Additional Book Endpoints
    
    * Refactored based on review
    
    * validation and emty CustomGlobalException class was added
    
    * Revert "Merge remote-tracking branch 'origin/GlobalExceptionHandler-and-data-validation' into GlobalExceptionHandler-and-data-validation"
    
    This reverts commit a0c81bcbbb9b5467160268357841ce2facc2ae19, reversing
    changes made to d9e1ac029f2f12601684c42fe94aac604f1b9117.
    
    * resolving conflicts
    
    * Added custom global exception handlers, deleted excessive annotation for dto validation
    
    - MethodArgumentNotValidException
    - EntityNotFoundException
    - SQLIntegrityConstraintViolationException
    
    * Refactored based on review
    
    * added @NotNull constraint for isbn field

--- src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/org/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,23 @@
 package org.example.bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "cannot be blank")
     private String title;
+    @NotBlank(message = "cannot be blank")
     private String author;
+    @ISBN(message = "Invalid ISBN format")
+    @NotNull(message = "cannot be null")
     private String isbn;
+    @NotNull(message = "cannot be null")
+    @PositiveOrZero(message = "must be positive or zero")
     private BigDecimal price;
     private String description;
     private String coverImage;
---
+++ src/main/java/org/example/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package org.example.bookstore.exception;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        List<String> errors =
+                ex.getBindingResult().getAllErrors().stream().map(this::getErrorMessage).toList();
+        return getResponseEntity(HttpStatus.BAD_REQUEST, errors);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return getResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<Object> handleSqlIntegrityConstraintViolationException(
+            SQLIntegrityConstraintViolationException ex) {
+        return getResponseEntity(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    private String getErrorMessage(ObjectError e) {
+        if (e instanceof FieldError fieldError) {
+            String field = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+            return String.format("%s: %s", field, message);
+        }
+        return e.getDefaultMessage();
+    }
+
+    private ResponseEntity<Object> getResponseEntity(HttpStatus status, Object errors) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.getReasonPhrase());
+        body.put("errors", errors);
+        return new ResponseEntity<>(body, status);
+    }
+}
