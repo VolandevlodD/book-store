0825eecf4cb34f2026a38609ce06efc1781518f1
VolandevlodD <vladradko2000@gmail.com>
2024-05-14 17:33:44 +0300

    checkstyle fixed

--- src/main/java/org/example/bookstore/controller/BookController.java
+++ src/main/java/org/example/bookstore/controller/BookController.java
@@ -11,7 +11,6 @@ import org.example.bookstore.service.BookService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
