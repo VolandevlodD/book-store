fc8f8514748a999d0b0f8a247569039a2897953a
VolandevlodD <vladradko2000@gmail.com>
2024-05-15 14:58:17 +0300

    refactored based on review

--- src/main/java/org/example/bookstore/security/JwtUtil.java
+++ src/main/java/org/example/bookstore/security/JwtUtil.java
@@ -39,7 +39,7 @@ public class JwtUtil {
                     .parseSignedClaims(token);
             return !claimsJws.getPayload().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
-            throw new JwtException("Invalid JWT token");
+            return false;
         }
     }
 
